e812b779b42d069aa591d8ac6b96305507fd7901
Enriquepardo <114728803+Enriquepardo@users.noreply.github.com>
2022-10-28 08:04:25 +0000

    añadido la carpeta de codigo 1

---
+++ main.py
@@ -0,0 +1,13 @@
+class Pato:
+  p = Pato()  
+  def hablar(self):
+    print("¡Cua!, Cua!")
+
+
+if __name__ == "__main__":
+p = Pato()
+p.hablar()
+# ¡Cua!, Cua!
+print ("Executed when invoked directly")
+else:
+    print ("Executed when imported")
